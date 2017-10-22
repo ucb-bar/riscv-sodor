f765a256d8579dd55789d37866809734e359a576
KRITIK BHIMANI <bhimanikritik@gmail.com>
2017-10-22 22:08:40 +0530

    bug fixed in ucode(microcode.scala) on executing instructions like "jalr ra, ra, -552"

--- src/rv32_ucode/microcode.scala
+++ src/rv32_ucode/microcode.scala
@@ -268,9 +268,9 @@ object Microcode
    /* PC <- A + B      */,                  Signals(Cat(MT_X , CSR.N, LDIR_0, RS_PC , RWR_1, REN_1, LDA_0, LDB_0, ALU_ADD  , AEN_1, LDMA_X, MWR_X, MEN_0, IS_X  , IEN_0, UBR_J),  "FETCH")
                                
    /* JALR             */ 
-   /* A  <- PC         */,Label("JALR"),    Signals(Cat(MT_X , CSR.N, LDIR_0, RS_PC , RWR_0, REN_1, LDA_1, LDB_X, ALU_X    , AEN_0, LDMA_X, MWR_X, MEN_0, IS_X  , IEN_0, UBR_N),  "X")
-   /* Reg[rd] <- A     */,                  Signals(Cat(MT_X , CSR.N, LDIR_0, RS_RD , RWR_1, REN_1, LDA_0, LDB_X, ALU_COPY_A,AEN_1, LDMA_X, MWR_X, MEN_0, IS_X  , IEN_0, UBR_N),  "X")
+   /* B  <- PC         */,Label("JALR"),    Signals(Cat(MT_X , CSR.N, LDIR_0, RS_PC , RWR_0, REN_1, LDA_0, LDB_1, ALU_X    , AEN_0, LDMA_X, MWR_X, MEN_0, IS_X  , IEN_0, UBR_N),  "X")
    /* A  <- Reg[rs1]   */,                  Signals(Cat(MT_X , CSR.N, LDIR_0, RS_RS1, RWR_0, REN_1, LDA_1, LDB_X, ALU_X    , AEN_0, LDMA_X, MWR_X, MEN_0, IS_X  , IEN_0, UBR_N),  "X")
+   /* Reg[rd] <- B     */,                  Signals(Cat(MT_X , CSR.N, LDIR_0, RS_RD , RWR_1, REN_1, LDA_0, LDB_X, ALU_COPY_B,AEN_1, LDMA_X, MWR_X, MEN_0, IS_X  , IEN_0, UBR_N),  "X")
    /* B  <- Sext(Imm12)*/,                  Signals(Cat(MT_X , CSR.N, LDIR_0, RS_X  , RWR_X, REN_0, LDA_0, LDB_1, ALU_X    , AEN_0, LDMA_X, MWR_X, MEN_0, IS_I  , IEN_1, UBR_N),  "X") 
    /* PC,A <- A + B    */,                  Signals(Cat(MT_X , CSR.N, LDIR_0, RS_PC , RWR_1, REN_1, LDA_1, LDB_0, ALU_ADD  , AEN_1, LDMA_X, MWR_X, MEN_0, IS_X  , IEN_0, UBR_J),  "FETCH")
       
